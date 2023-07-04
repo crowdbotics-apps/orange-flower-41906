644608cc5cd2b638ece684cebbe4c508ad70403e
Crowdbotics <team@crowdbotics.com>
2023-07-04 22:12:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "orange-flower-41906",
+  url: "https://orange-flower-41906.botics.co",
+  api: "https://orange-flower-41906.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
